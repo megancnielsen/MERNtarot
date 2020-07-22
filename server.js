90633dafdb3f21be0c78dff2d5e0caf9b649bced
IFletcher <Isiahfletcher7@gmail.com>
2020-07-21 21:30:54 -0600

    cleaned server.js and moved base api route to appropriate module

--- server.js
+++ server.js
@@ -4,11 +4,10 @@ const port = 8000;
 
 require('./server/config/mongoose.config');
 
+app.use(express.json(), express.urlencoded({ extended: true }));
+
+// sends express() to routes module
+const AllApiRoutes = require('./server/routes/appointment.routes');
+AllApiRoutes(app);
+
 app.listen(port, () => console.log(`I'm in on port ${port}`));
-
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello World" });
-});
-
-app.use( express.json() );
-app.use( express.urlencoded({ extended: true }) );
---
+++ server/routes/appointment.routes.js
@@ -0,0 +1,9 @@
+app.get('/api', (req, res) => {
+    res.json({ message: 'Hello World' });
+});
+
+app.post('/api/appointmentSchema', (req, res) => {
+    console.log(req.body);
+    appointmentSchema.push(req.body);
+    res.json({ status: 'ok' });
+});
