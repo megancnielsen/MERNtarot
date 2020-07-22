9057b49ee8d33936c515e45543d7c6efa13f0973
IFletcher <Isiahfletcher7@gmail.com>
2020-07-21 21:52:51 -0600

    added get route

--- server/routes/appointment.route.js
+++ server/routes/appointment.route.js
@@ -1,5 +1,9 @@
-app.post("/api/appointmentSchema", (req, res) => {
+app.get('/api', (req, res) => {
+    res.json({ message: 'Hello World' });
+});
+
+app.post('/api/appointmentSchema', (req, res) => {
     console.log(req.body);
     appointmentSchema.push(req.body);
-    res.json( { status: "ok" } );
+    res.json({ status: 'ok' });
 });
